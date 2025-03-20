3fda139cc007e8cec69e861fe6ce572556bf9de8
Phạm Trí Nhân <114942828+TriNhan-Pham@users.noreply.github.com>
2025-03-21 08:11:36 +1000

    retry with no sleep dependence

--- .csse6400/bin/docker-compose.sh
+++ .csse6400/bin/docker-compose.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
 # Check that the health endpoint is returning 200 using docker-compose
-sudo app install docker-compose
-docker-compose build
+sudo app install docker compose
+docker compose build
 
-docker-compose up -d
+docker compose up -d
 error=$?
 pid=$!
 if [[ $error -ne 0 ]]; then
@@ -23,5 +23,5 @@ if [[ $error -ne 0 ]]; then
     exit 1
 fi
 
-docker-compose down
+docker compose down
 
